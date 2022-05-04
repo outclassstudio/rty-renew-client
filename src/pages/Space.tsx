c8c700df5e788907e65d26b334c79c4f179f39ef
이민형 <mhlee@wemb.co.kr>
2022-05-04 15:54:10 +0900

    set canvas

--- src/pages/Space.tsx
+++ src/pages/Space.tsx
@@ -1,5 +1,48 @@
-export default function Space () {
+import styled from "styled-components";
+import Paper from "paper";
+import { useEffect, useRef } from "react";
+
+export const MainDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+`;
+
+export const Canvas = styled.canvas`
+  width: 100%;
+  height: 100%;
+  background: #f6f6f6;
+`;
+
+export default function Space() {
+  const canvasRef = useRef(null);
+
+  const draw = () => {
+    let myPath: any;
+
+    Paper.view.onMouseDown = () => {
+      myPath = new Paper.Path();
+      myPath.strokeColor = "black";
+      myPath.strokeWidth = 3;
+    };
+
+    Paper.view.onMouseDrag = (event: any) => {
+      myPath.add(event.point);
+    };
+  };
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      Paper.setup(canvasRef.current);
+    }
+  }, []);
+
+  useEffect(() => {
+    draw();
+  }, [draw]);
+
   return (
-    <div></div>
-  )
+    <MainDiv>
+      <Canvas ref={canvasRef}></Canvas>
+    </MainDiv>
+  );
 }
