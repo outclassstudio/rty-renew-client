3ee32c292c3e856616a80b1354f2aa0859aeb6ff
Minhyeong Lee <mhlee@wemb.co.kr>
2022-05-30 11:16:59 +0900

    Feat : Improve UI/UX

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -149,9 +149,10 @@ export default function Signup() {
           });
         })
         .catch((err) => {
+          console.log("에러메시지", err);
           Swal.fire({
             title: "입력을 확인해주세요",
-            text: `${err}`,
+            text: `${err.response.data}`,
             icon: "warning",
             confirmButtonText: "닫기",
           });
